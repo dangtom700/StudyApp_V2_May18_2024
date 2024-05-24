1b6beef664f7ff452c414c24c203dbf342219f31
Azalea1047 <azalea1047@gmail.com>
2024-05-24 12:44:13 -0700

    set the frame to updateData

--- source/modules/updateData.py
+++ source/modules/updateData.py
@@ -10,3 +10,24 @@
 # - list of note files
 # - output to database
 # - Reset database option
+import modules.path as path
+import os
+import sqlite3
+
+def getFileList(path: str, fileType: str) -> list[str]:
+    return [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f)) and f.endswith(fileType)]
+
+def processPDFfilesIntoDB(PDFlist: list[str], db_name: str, chunk_size: int, resetDB: bool) -> None:
+    pass
+
+def processNoteFilesIntoDB(noteList: list[str], db_name: str, resetDB: bool) -> None:
+    pass
+
+def updateData():
+    PDFlist = getFileList(path.PDFpath, ".pdf")
+    db_name = path.DB_name
+    chunk_size = 1000
+    resetDB = True
+    processPDFfilesIntoDB(PDFlist, db_name, chunk_size, resetDB)
+    noteList = getFileList(path.notePath, ".md")
+    processNoteFilesIntoDB(noteList, db_name, resetDB)
