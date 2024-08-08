58a1eb7ccdeba5b1e0397f8995559df5fed9e655
Azalea1047 <dangtom700@gmail.com>
2024-08-07 21:31:10 -0700

    get rid of temporary functions

--- source/modules/updateLog.py
+++ source/modules/updateLog.py
@@ -1,6 +1,6 @@
 import sqlite3
 import time
-from modules.path import log_database_path, chunk_database_path, pdf_path
+from modules.path import log_database_path, chunk_database_path, ReadingMaterial_path
 from os import listdir, makedirs
 from os.path import isdir, basename, join, exists
 import shutil
@@ -32,29 +32,8 @@ def store_log_file_to_database(log_file_path: str) -> None:
     # empty_log_file
     with open(log_file_path, 'w') as log_file:
         pass
-def create_cs_file(source_path: str, extension: str) -> None:
 
-    if not isdir(source_path):
-        return None
-
-    path = {".md": "data/note_list.md", 
-            ".pdf": "data/pdf_list.md"}
-    with open(path[extension], 'w') as f:
-        for file in listdir(source_path):
-            if file.endswith(extension):
-                f.write(basename(file).removesuffix(extension) + ', ')
-
-def categorize_pdf_files_by_theme(source_path: str) -> None:
-    categories = {
-    "Computer Science and Engineering": ["Algorithms and Data Structures", "Programming Languages and Software Development", "Database Systems and Management", "Computer Architecture and Organization", "Computer Networks and Security", "Artificial Intelligence and Machine Learning", "Robotics and Automation", "Computer Graphics and Visualization", "Embedded Systems and IoT"],
-    "Mathematics and Statistics": ["Statistics and Probability", "Mathematical Modeling and Optimization", "Numerical Methods", "Discrete Mathematics", "Game Theory"],
-    "Business and Economics": ["Economics and Econometrics", "Finance and Accounting", "Management and Organizational Behavior", "Marketing and Business Analytics", "Operations Research and Supply Chain"],
-    "Engineering (General)": ["Engineering Mechanics and Materials", "Thermodynamics and Fluid Mechanics", "Electrical Engineering", "Control Systems and Automation", "Signal Processing and Communication", "Energy and Environment"],
-    "Science and Technology": ["Physics", "Chemistry", "Biology", "Nanotechnology", "Materials Science", "Agriculture and Food Science"],
-    "Social Sciences":["Philosophy and Ethics", "History and Social Sciences", "Design and Human-Computer Interaction"]
-    }
-
-def categorize_pdf_files_by_month_year(destination_path = "D:\\ReadingMaterial") -> None:
+def categorize_pdf_files_by_month_year(destination_path = ReadingMaterial_path) -> None:
     def convert_date(date: str) -> str:
         # Sun, Apr 14, 2024, 22:21:05 to create 2024-04 as a folder to sort files
         date = date.split(", ")
