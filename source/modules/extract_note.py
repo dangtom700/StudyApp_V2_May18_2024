098f8a5ff5c55ad4d4930b45227f4573214e8bb2
Azalea1047 <dangtom700@gmail.com>
2024-08-19 10:54:04 -0700

    integrate into the code

--- source/modules/extract_note.py
+++ source/modules/extract_note.py
@@ -11,7 +11,7 @@ from time import ctime
 from modules.updateLog import log_message
 from modules.path import chunk_database_path
 from modules.extract_pdf import batch_collect_files, store_chunks_in_db
-from typing import List
+from typing import Generator
 
 def get_updated_time(file_path: str) -> str:
     """
@@ -176,7 +176,7 @@ def process_markdown_file(file_path, CHUNK_SIZE = 800) -> None:
             chunks = [text[i:i + CHUNK_SIZE] for i in range(0, len(text), CHUNK_SIZE)]
             store_text_note_in_chunks_with_retry(file_name=os.path.basename(file_path), chunks=chunks, db_name=chunk_database_path)
 
-def process_text_note_batch_of_files(file_batch: List[str], chunk_size = 800) -> None:
+def process_text_note_batch_of_files(file_batch: list[str], chunk_size = 800) -> None:
     """Processes a batch of markdown files concurrently."""
     threads = []
     
@@ -197,3 +197,68 @@ def extract_markdown_notes_in_batches(directory, chunk_size = 800) -> None:
 
     print("Finished processing markdown files.")
     log_message("Finished processing markdown files.")
+
+def batch_collect_words(cursor, batch_size=975) -> Generator[list[str], None, None]:
+    offset = 0
+    while True:
+        cursor.execute("SELECT word FROM coverage_analysis LIMIT ? OFFSET ?", (batch_size, offset))
+        extracted_words = [word[0] for word in cursor.fetchall()]
+
+        if not extracted_words:
+            break
+
+        yield extracted_words
+        offset += batch_size
+
+def setup_tables(cursor: sqlite3.Cursor, number_of_tables: int) -> None:
+    # Drop all tables
+    for index in range(number_of_tables):
+        cursor.execute(f"DROP TABLE IF EXISTS IF_IDF_table_{index}")
+        print(f"Dropped table IF_IDF_table_{index}")
+        log_message(f"Dropped table IF_IDF_table_{index}")
+
+    # Create tables
+    for index, word_list in zip(range(number_of_tables), batch_collect_words(cursor)):
+        cursor.execute(f"CREATE TABLE IF_IDF_table_{index} (word TEXT PRIMARY KEY, FOREIGN KEY(word) REFERENCES coverage_analysis(word))")
+        cursor.execute(f"INSERT INTO IF_IDF_table_{index} SELECT word FROM coverage_analysis")
+        for keyword in word_list:
+            cursor.execute(f"ALTER TABLE IF_IDF_table_{index} ADD COLUMN '{keyword}_count' INTEGER DEFAULT 0")
+            cursor.execute(f"ALTER TABLE IF_IDF_table_{index} ADD COLUMN '{keyword}_TF_IDF' REAL DEFAULT 0.0")
+        print(f"Created table IF_IDF_table_{index} with {len(word_list)} keywords")
+        log_message(f"Created table IF_IDF_table_{index} with {len(word_list)} keywords")
+
+    # Create word_impact_titles table
+    cursor.execute("DROP TABLE IF EXISTS TF_IDF_titles")
+    print("Dropped table TF_IDF_titles")
+    log_message("Dropped table TF_IDF_titles")
+    cursor.execute("""CREATE TABLE TF_IDF_titles (
+                word TEXT PRIMARY KEY,
+                FOREIGN KEY(word) REFERENCES coverage_analysis(word)
+                )""")
+    title_collection = cursor.execute("SELECT id FROM file_list WHERE chunk_count > 0 AND file_type = 'pdf'").fetchall()
+    cleaned_title_collection = [title[0] for title in title_collection]
+    for title in cleaned_title_collection:
+        cursor.execute(f"ALTER TABLE TF_IDF_titles ADD COLUMN '{title}_count' INTEGER DEFAULT 0")
+        cursor.execute(f"ALTER TABLE TF_IDF_titles ADD COLUMN '{title}_TF_IDF' REAL DEFAULT 0.0")
+    print(f"Created table TF_IDF_titles with {len(cleaned_title_collection)} titles")
+    log_message(f"Created table TF_IDF_titles with {len(cleaned_title_collection)} titles")
+    # Complete transaction
+    cursor.connection.commit()
+
+def compute_tf_idf_text_chunk(database_path: str) -> None:
+    conn = sqlite3.connect(database_path)
+    cursor = conn.cursor()
+
+    NUMER_OF_WORDS = cursor.execute("SELECT COUNT(*) FROM coverage_analysis").fetchone()[0]
+    BATCH_SIZE = 975
+    # Round up the number of tables
+    number_of_tables = (NUMER_OF_WORDS + BATCH_SIZE - 1) // BATCH_SIZE
+
+    # Setup tables
+    print("Setting relational tables for computing TF-IDF")
+    log_message("Setting relational tables for computing TF-IDF")
+    
+    setup_tables(cursor, number_of_tables)
+
+    # Process text chunks
+    print("Computing TF-IDF for text chunks")
