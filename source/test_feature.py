fe6e9440e431039349c02d27cf768c3faf7f7480
Azalea1047 <dangtom700@gmail.com>
2024-08-21 11:55:38 -0700

    complete table creation for computation

--- source/test_feature.py
+++ source/test_feature.py
@@ -28,17 +28,10 @@ import sqlite3
 import modules.path as path
 from typing import Generator
 
-def count_one_term_appear_in_each_chunk(term, text_chunk) -> int:
-    """
-    Count the number of times the term appears in the text chunk.
-    """
-    text_chunk = text_chunk.lower()
-    return text_chunk.count(term.lower())
-
-def batch_collect_words(cursor, batch_size=975) -> Generator[list[str], None, None]:
+def batch_collect_words(cursor, batch_size=975, column_name="word", table_name="coverage_analysis") -> Generator[list[str], None, None]:
     offset = 0
     while True:
-        cursor.execute("SELECT word FROM coverage_analysis LIMIT ? OFFSET ?", (batch_size, offset))
+        cursor.execute(f"SELECT {column_name} FROM {table_name} LIMIT ? OFFSET ?", (batch_size, offset))
         extracted_words = [word[0] for word in cursor.fetchall()]
 
         if not extracted_words:
@@ -47,7 +40,7 @@ def batch_collect_words(cursor, batch_size=975) -> Generator[list[str], None, No
         yield extracted_words
         offset += batch_size
 
-def setup_tables(cursor: sqlite3.Cursor, number_of_tables: int) -> None:
+def setup_TF_IDF_tables(cursor: sqlite3.Cursor, number_of_tables: int) -> None:
     # Drop all tables
     for index in range(number_of_tables):
         cursor.execute(f"DROP TABLE IF EXISTS TF_IDF_table_{index}")
@@ -55,8 +48,8 @@ def setup_tables(cursor: sqlite3.Cursor, number_of_tables: int) -> None:
 
     # Create tables
     for index, word_list in zip(range(number_of_tables), batch_collect_words(cursor)):
-        cursor.execute(f"CREATE TABLE TF_IDF_table_{index} (word TEXT PRIMARY KEY, FOREIGN KEY(word) REFERENCES coverage_analysis(word))")
-        cursor.execute(f"INSERT INTO TF_IDF_table_{index} SELECT word FROM coverage_analysis")
+        cursor.execute(f"CREATE TABLE TF_IDF_table_{index} (chunk_text TEXT PRIMARY KEY, FOREIGN KEY(chunk_text) REFERENCES pdf_chunks(chunk_text))")
+        cursor.execute(f"INSERT INTO TF_IDF_table_{index} SELECT chunk_text FROM pdf_chunks")
         for keyword in word_list:
             cursor.execute(f"ALTER TABLE TF_IDF_table_{index} ADD COLUMN '{keyword}_count' INTEGER DEFAULT 0")
             cursor.execute(f"ALTER TABLE TF_IDF_table_{index} ADD COLUMN '{keyword}_TF_IDF' REAL DEFAULT 0.0")
@@ -69,6 +62,7 @@ def setup_tables(cursor: sqlite3.Cursor, number_of_tables: int) -> None:
                 word TEXT PRIMARY KEY,
                 FOREIGN KEY(word) REFERENCES coverage_analysis(word)
                 )""")
+    cursor.execute("INSERT INTO TF_IDF_titles SELECT word FROM coverage_analysis")
     title_collection = cursor.execute("SELECT id FROM file_list WHERE chunk_count > 0 AND file_type = 'pdf'").fetchall()
     cleaned_title_collection = [title[0] for title in title_collection]
     for title in cleaned_title_collection:
@@ -78,7 +72,7 @@ def setup_tables(cursor: sqlite3.Cursor, number_of_tables: int) -> None:
     # Complete transaction
     cursor.connection.commit()
 
-def compute_tf_idf_text_chunk(database_path: str) -> None:
+def main_function(database_path: str) -> None:
     conn = sqlite3.connect(database_path)
     cursor = conn.cursor()
 
@@ -88,6 +82,6 @@ def compute_tf_idf_text_chunk(database_path: str) -> None:
     number_of_tables = (NUMER_OF_WORDS + BATCH_SIZE - 1) // BATCH_SIZE
 
     # Setup tables
-    setup_tables(cursor, number_of_tables)
+    setup_TF_IDF_tables(cursor, number_of_tables)
 
-compute_tf_idf_text_chunk(path.chunk_database_path)
+main_function(path.chunk_database_path)
