835c8fdd3b87ce02c091bead416cd2b8398ac361
Azalea1047 <dangtom700@gmail.com>
2024-08-29 12:02:46 -0700

    test computing TF-IDF

--- source/test_feature.py
+++ source/test_feature.py
@@ -1,6 +1,6 @@
 import sqlite3
 import modules.path as path
-from modules.updateLog import print_and_log, log_message
+from modules.updateLog import print_and_log
 from modules.extract_pdf import get_title_ids, retrieve_token_list
 from math import log, sqrt
 from datetime import datetime
@@ -215,6 +215,32 @@ def precompute_title_vector(database_path: str) -> None:
                     (SELECT T_{title} FROM title_analysis WHERE title_normalized.word = title_analysis.word) / ?
             """, (length,))
         conn.commit()
+
+    def compute_TF_IDF(title_ids: list[str]) -> None:
+        TOTAL_DOC = len(title_ids)
+        for title in title_ids:
+            total_terms = cursor.execute(f"SELECT SUM(T_{title}) FROM title_analysis").fetchone()[0]
+            if total_terms == 0:
+                continue  # Skip if no terms are found
+            
+            term_counts = cursor.execute(f"SELECT word, T_{title} FROM title_analysis").fetchall()
+            
+            for word, term_count in term_counts:
+                if term_count == 0:
+                    continue
+                
+                tf = term_count / total_terms
+                doc_term = cursor.execute(f"""
+                    SELECT * 
+                    FROM title_analysis 
+                    WHERE word = ?
+                """, (word,)).fetchall()[0]
+                doc_with_term = len([count for count in doc_term[1:] if count > 0])
+                
+                idf = log(TOTAL_DOC / (1 + doc_with_term))
+                cursor.execute(f"""
+                    UPDATE title_tf_idf 
+                    SET T_{title} = ?""", (tf * idf,))
     
     def get_words() -> list[str]:
         cursor.execute("SELECT word FROM coverage_analysis")
@@ -224,19 +250,25 @@ def precompute_title_vector(database_path: str) -> None:
     start_execution_time = datetime.now()
 
     title_ids = get_title_ids(cursor=cursor)
-    word_essentials = get_words()
-    create_tables(title_ids=title_ids)
-
-    # order pdf_chunks by id
-    cursor.execute("SELECT id FROM pdf_chunks ORDER BY id ASC")
-    # main processing
-    print_and_log("Processing title analysis...")
-    process_title_analysis(title_ids=title_ids, words=word_essentials, cursor=cursor)
-    print_and_log("Finished processing title analysis.")
-
-    print_and_log("Normalizing vectors...")
-    normalize_vector(title_ids=title_ids)
-    print_and_log("Finished normalizing vectors.")
+    # word_essentials = get_words()
+    
+    # print_and_log("Creating tables...")
+    # create_tables(title_ids=title_ids)
+    # print_and_log("Finished creating tables.")
+    # # order pdf_chunks by id
+    # cursor.execute("SELECT id FROM pdf_chunks ORDER BY id ASC")
+    # # counting
+    # print_and_log("Processing title analysis...")
+    # process_title_analysis(title_ids=title_ids, words=word_essentials, cursor=cursor)
+    # print_and_log("Finished processing title analysis.")
+    # # normalizing
+    # print_and_log("Normalizing vectors...")
+    # normalize_vector(title_ids=title_ids)
+    # print_and_log("Finished normalizing vectors.")
+    # computing TF-IDF
+    print_and_log("Computing TF-IDF...")
+    compute_TF_IDF(title_ids=title_ids)
+    print_and_log("Finished computing TF-IDF.")
 
     conn.commit()
     conn.close()
