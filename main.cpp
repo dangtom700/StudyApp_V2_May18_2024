2f31b0be80ff2897e59e60819b8d9a97f465e2fd
Azalea1047 <dangtom700@gmail.com>
2024-08-31 13:03:35 -0700

    add const env_path

--- main.cpp
+++ main.cpp
@@ -5,11 +5,18 @@
 #include "lib/wor_frq.hpp"
 
 #include<chrono>
+#include<filesystem>
+#include<string>
+
+const std::string StudyApp_root_path = std::filesystem::current_path().string(); // get current working directory
+const PATH path = ENV_VAR_HPP::PATH(StudyApp_root_path);
 
 int main() {
     std::chrono::time_point<std::chrono::system_clock> start_time = std::chrono::system_clock::now();
+
     INTERFACE_HPP::Greeting();
     INTERFACE_HPP::Get_Request();
+
     INTERFACE_HPP::Print_Operating_Time(start_time);
     return 0;
 }
