c45e700af5862a7895eb8e4c1309f28e8062f98b
Azalea1047 <dangtom700@gmail.com>
2024-09-01 17:40:10 -0700

    test main flow

--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,19 @@
 #include "lib/vec_com.hpp"
 #include "lib/wor_frq.hpp"
 #include "lib/search.hpp"
-#include "lib/db.hpp"
+// #include "lib/db.hpp"
 
 #include<chrono>
 #include<string>
+#include<map>
 
 const PATH path = ENV_VAR_HPP::PATH();
 
 int main() {
 
-    // INTERFACE_HPP::Greeting();
-    // INTERFACE_HPP::Get_Request();
-    DATABASE_HPP::initialize_database("data/pdf_db.db");
+    INTERFACE_HPP::Greeting();
+    std::map <int, std::string> collected_request = INTERFACE_HPP::Get_Request();
+    INTERFACE_HPP::Coordinate_Request(collected_request);
     
     return 0;
 }
