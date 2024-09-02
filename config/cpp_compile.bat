f7710e604815879ba342ae6866a1c6ab820b56b5
Azalea1047 <dangtom700@gmail.com>
2024-09-01 17:39:42 -0700

    fix the time

--- config/cpp_compile.bat
+++ config/cpp_compile.bat
@@ -6,21 +6,29 @@ g++ -o StudyLogDB src\main.cpp -l sqlite3
 
 REM Get the start time
 for /F "tokens=1-4 delims=:.," %%a in ("%time%") do (
-    set /A startTimeInSeconds=(((%%a*3600) + (%%b*60) + %%c)*100 + %%d)
+    set /A startTimeInSeconds=%%a*3600 + %%b*60 + %%c
+    set startMilliseconds=%%d
 )
 
-REM Run the compiled program
-StudyLogDB.exe
+REM Run the constructed command
+%cmd%
 
 REM Get the end time
 for /F "tokens=1-4 delims=:.," %%a in ("%time%") do (
-    set /A endTimeInSeconds=(((%%a*3600) + (%%b*60) + %%c)*100 + %%d)
+    set /A endTimeInSeconds=%%a*3600 + %%b*60 + %%c
+    set endMilliseconds=%%d
 )
 
-REM Calculate the elapsed time in milliseconds
+REM Calculate the elapsed time
 set /A elapsedTime=endTimeInSeconds-startTimeInSeconds
-set /A elapsedMilliseconds=elapsedTime %% 100
-set /A elapsedTime=elapsedTime / 100
+set /A elapsedMilliseconds=endMilliseconds-startMilliseconds
+
+REM Adjust milliseconds if necessary
+if %elapsedMilliseconds% lss 0 (
+    set /A elapsedMilliseconds+=100
+    set /A elapsedTime-=1
+)
+
 set /A elapsedSeconds=elapsedTime %% 60
 set /A elapsedTime=elapsedTime / 60
 set /A elapsedMinutes=elapsedTime %% 60
