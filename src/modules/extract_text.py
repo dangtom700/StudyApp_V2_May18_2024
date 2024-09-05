88cff05811094cfea2a5dd0f4f6db298e802d738
Azalea1047 <dangtom700@gmail.com>
2024-09-05 15:15:33 -0700

    comment suggestion based on precomputation

--- src/modules/extract_text.py
+++ src/modules/extract_text.py
@@ -374,44 +374,44 @@ def process_word_frequencies_in_batches():
 #     conn.commit()
 #     conn.close()
 
-def suggest_top_titles(database_path: str, prompt: str, top_n = 10):
-    conn = sqlite3.connect(database_path)
-    cursor = conn.cursor()
+# def suggest_top_titles(database_path: str, prompt: str, top_n = 10):
+#     conn = sqlite3.connect(database_path)
+#     cursor = conn.cursor()
 
-    prompt = clean_text(prompt)
+#     prompt = clean_text(prompt)
 
-    cursor.execute("SELECT word FROM coverage_analysis")
-    words = cursor.fetchall()
-    words = {word[0]: 0 for word in words}
+#     cursor.execute("SELECT word FROM coverage_analysis")
+#     words = cursor.fetchall()
+#     words = {word[0]: 0 for word in words}
 
-    for token in words.keys():
-        words[token] = prompt.count(token)
+#     for token in words.keys():
+#         words[token] = prompt.count(token)
 
-    length_prompt = sqrt(sum([value * value for value in words.values()]))
+#     length_prompt = sqrt(sum([value * value for value in words.values()]))
 
-    normalized_prompt = {key: value / length_prompt for key, value in words.items()}
+#     normalized_prompt = {key: value / length_prompt for key, value in words.items()}
 
-    title_list = cursor.execute("SELECT id FROM file_list WHERE file_type = 'pdf' AND chunk_count > 0").fetchall()
-    # create an array of zeros
-    title_list = {name[0]: 0 for name in title_list}
+#     title_list = cursor.execute("SELECT id FROM file_list WHERE file_type = 'pdf' AND chunk_count > 0").fetchall()
+#     # create an array of zeros
+#     title_list = {name[0]: 0 for name in title_list}
 
-    # get non-zero keys
-    key_list = [key for key, value in normalized_prompt.items() if value != 0]
+#     # get non-zero keys
+#     key_list = [key for key, value in normalized_prompt.items() if value != 0]
 
-    # get values
-    for title in title_list.keys():
-        for key in key_list:
-            cursor.execute(f"SELECT T_{title} FROM title_normalized WHERE word = ?", (key,))
-            title_list[title] += cursor.fetchone()[0] * normalized_prompt[key]
+#     # get values
+#     for title in title_list.keys():
+#         for key in key_list:
+#             cursor.execute(f"SELECT T_{title} FROM title_normalized WHERE word = ?", (key,))
+#             title_list[title] += cursor.fetchone()[0] * normalized_prompt[key]
 
-    top_10 = sorted(title_list.items(), key=lambda x: x[1], reverse=True)[:top_n]
+#     top_10 = sorted(title_list.items(), key=lambda x: x[1], reverse=True)[:top_n]
 
-    # Look up the name of the top 10 titles
-    for title, score in top_10:
-        cursor.execute("SELECT file_name FROM file_list WHERE id = ?", (title,))
-        print(f"{score:.18f}: {title} - {cursor.fetchone()[0]}")
+#     # Look up the name of the top 10 titles
+#     for title, score in top_10:
+#         cursor.execute("SELECT file_name FROM file_list WHERE id = ?", (title,))
+#         print(f"{score:.18f}: {title} - {cursor.fetchone()[0]}")
 
-    conn.close()
+#     conn.close()
 
 def get_modification_time(file_path: str) -> tuple[str, int]:
     # Get the modification time in seconds since EPOCH
