e76555539096f2bd8456d2ac15bac6c7c3d46d20
Azalea1047 <dangtom700@gmail.com>
2024-09-06 09:44:01 -0700

    implement normalize logic

--- src/test_feature.py
+++ src/test_feature.py
@@ -1,13 +1,14 @@
 import sqlite3
-import os
-from modules.path import chunk_database_path, token_json_path
+from os import listdir
+from math import sqrt
+from modules.path import token_json_path, chunk_database_path
 from concurrent.futures import ThreadPoolExecutor
 from modules.updateLog import print_and_log
 
 def get_title_ids(path:str) -> list[str]:
-    return [title.removesuffix(".json") for title in os.listdir(path)]
+    return [title.removesuffix(".json") for title in listdir(path)]
 
-def precompute_title_analysis(database_path: str, title_id: str):
+def precompute_title_analysis(database_path: str, title_id: str, words: list[str]):
     conn = sqlite3.connect(database_path)
     cursor = conn.cursor()
 
@@ -20,7 +21,13 @@ def normalize_vector(database_path: str, title_id:str):
     conn = sqlite3.connect(database_path)
     cursor = conn.cursor()
 
-    # logic goes here
+    length = cursor.execute(f"SELECT SUM(T_{title_id} * T_{title_id}) FROM title_analysis").fetchone()[0]
+    length = sqrt(length)
+    cursor.execute(f"""
+        UPDATE title_normalized
+            SET T_{title_id} = 
+                (SELECT T_{title_id} FROM title_analysis WHERE title_normalized.word = title_analysis.word) /(1 + {length})
+    """)
 
     conn.commit()
     conn.close()
@@ -30,6 +37,8 @@ def vectorize_title(database_path: str): # Main flow
     cursor = conn.cursor()
 
     title_ids = get_title_ids(path=token_json_path)
+    words = cursor.execute("SELECT word FROM coverage_analysis").fetchall()
+    words = [word[0] for word in words]
 
     def create_table(title_ids = title_ids):
         command_fill = ', '.join([f"T_{title_id} INTEGER DEFAULT 0" for title_id in title_ids])
@@ -54,5 +63,8 @@ def vectorize_title(database_path: str): # Main flow
             print_and_log(f"Normalizing title {title_id}...")
             executor.submit(normalize_vector, database_path=database_path, title_id=title_id)
 
+    print_and_log("All titles vectorized.")
     conn.commit()
     conn.close()
+
+vectorize_title(database_path=chunk_database_path)
