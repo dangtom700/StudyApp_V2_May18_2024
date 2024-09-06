222c6f7e0fbf1648295403c248811e3cd7fd5329
Azalea1047 <dangtom700@gmail.com>
2024-09-05 20:10:14 -0700

    reorganize logic for updateDB

--- src/modules/updateDB.py
+++ src/modules/updateDB.py
@@ -14,27 +14,6 @@ def get_modification_time(file_path: str) -> tuple[str, int]:
     epoch_time = int(modification_time)
     return (formatted_modification_time, epoch_time)
 
-def setup_database(reset_db: bool, db_name: str) -> None:
-    conn = sqlite3.connect(db_name)
-    cursor = conn.cursor()
-    
-    if reset_db:
-        cursor.execute(f"DROP TABLE IF EXISTS file_list")
-    cursor.execute(f"""CREATE TABLE IF NOT EXISTS file_list (
-                   id TEXT PRIMARY KEY, 
-                   file_name TEXT, 
-                   file_path TEXT, 
-                   file_type TEXT,
-                   created_time TEXT, 
-                   epoch_time INTEGER,
-                   chunk_count INTEGER,
-                   start_id INTEGER,
-                   end_id INTEGER
-                   )""")
-
-    conn.commit()
-    conn.close()
-
 def create_unique_id(file_basename: str, epoch_time: int, chunk_count: int, starting_id: int) -> str:
     # Step 1: Encode the file basename
     # Sum the ASCII values of all characters, XOR by 1600, and apply & 0xFFFF
@@ -66,32 +45,28 @@ def count_chunk_for_each_title(cursor: sqlite3.Cursor, file_name: str) -> int:
     # print(f"Chunk count for {file_name}: {chunk_count}")
     return chunk_count
 
-def get_starting_and_ending_ids(cursor: sqlite3.Cursor, file_name: str) -> tuple[int, int]:
+def get_starting_and_ending_ids(cursor: sqlite3.Cursor, file_name: str) -> int:
     # Execute a single query to get both the starting and ending IDs
     cursor.execute('''
-        SELECT MIN(id) AS starting_id, MAX(id) AS ending_id
+        SELECT MIN(id) AS starting_id
         FROM pdf_chunks
         WHERE file_name = ?;
     ''', (file_name,))
     
-    result = cursor.fetchone()
-    
-    starting_id, ending_id = result
-    # print(f"Starting ID: {starting_id}, Ending ID: {ending_id}")
-    return starting_id, ending_id
+    return cursor.fetchone()[0]
 
-def store_files_in_db(file_names: list[str], file_list: list[str], db_name: str, file_type: str) -> None:
-    conn = sqlite3.connect(db_name)
-    cursor = conn.cursor()
+def store_files_in_db(file_names: list[str], 
+                      file_list: list[str], 
+                      file_type: str, 
+                      conn: sqlite3.Connection, 
+                      cursor: sqlite3.Cursor) -> None:
     
     for file_name, file_path in zip(file_names, file_list):
         created_time, epoch_time = get_modification_time(file_path)
         file_basename = basename(file_path)
         chunk_count = count_chunk_for_each_title(cursor, file_name=file_basename)
-        starting_id, ending_id = get_starting_and_ending_ids(cursor, file_name=file_basename)
-        if starting_id is None or ending_id is None:
-            starting_id = 0
-            ending_id = 0
+        starting_id = get_starting_and_ending_ids(cursor, file_name=file_basename)
+        starting_id = starting_id if starting_id is not None else 0
         hashed_data = create_unique_id(file_basename, epoch_time, chunk_count, starting_id)
         
         cursor.execute(f"""INSERT INTO file_list (
@@ -102,25 +77,38 @@ def store_files_in_db(file_names: list[str], file_list: list[str], db_name: str,
             created_time,
             epoch_time,
             chunk_count,
-            start_id,
-            end_id
+            start_id
             ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)""",
-            (hashed_data, file_name, file_path, file_type, created_time, epoch_time, chunk_count, starting_id, ending_id)
+            (hashed_data, file_name, file_path, file_type, created_time, epoch_time, chunk_count, starting_id,)
         )
     conn.commit()
-    conn.close()
 # Main function
 def extract_names(raw_list: list[str], extension: list[str]) -> list[str]:
     return [basename(file).removesuffix(extension) for file in raw_list if file.endswith(extension)]
 
 def create_type_index_table(collector_folder_list: list[str], extension_list: list[str]) -> None:
     print_and_log(f"Started creating file index.")
-    
-    # Initialize database
-    setup_database(reset_db=True, db_name=chunk_database_path)
-    
+    conn = sqlite3.connect(chunk_database_path)
+    cursor = conn.cursor()
+
+    def create_table():
+        cursor.execute("DROP TABLE IF EXISTS file_list")
+        cursor.execute("""CREATE TABLE file_list (
+            id TEXT PRIMARY KEY,
+            file_name TEXT,
+            file_path TEXT,
+            file_type TEXT,
+            created_time TEXT,
+            epoch_time INTEGER DEFAULT 0,
+            chunk_count INTEGER DEFAULT 0,
+            start_id INTEGER DEFAULT 0,
+        )""")
+
+    print_and_log("Started creating table.")
+    create_table()
     print_and_log("Started storing files in database.")
     for collector_folder, extension in zip(collector_folder_list, extension_list):
+        # Each folder has a different type of files
         for file_batch in batch_collect_files(folder_path=collector_folder, extension=extension, batch_size=100):
             file_names = extract_names(file_batch, extension)
             
@@ -128,8 +116,8 @@ def create_type_index_table(collector_folder_list: list[str], extension_list: li
                 print_and_log(f"Processing file: {file_name}...")
                 store_files_in_db(file_names=[file_name], 
                                   file_list=[file_path_with_extension], 
-                                  db_name=chunk_database_path, 
-                                  file_type=extension.removeprefix("."))
-
-    print_and_log(f"Files: file stored in database.")
-    print_and_log(f"Processing complete: create file index.")
+                                  file_type=extension.removeprefix("."),
+                                  conn=conn, 
+                                  cursor=cursor)
+    conn.commit()
+    conn.close()
