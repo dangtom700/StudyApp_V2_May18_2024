e343daa095b08a80fd47a2b8fb330fabbd3bd35f
Azalea1047 <dangtom700@gmail.com>
2024-09-29 21:27:24 -0700

    udpate set up file

--- config/set_up.bat
+++ config/set_up.bat
@@ -2,6 +2,6 @@
 conda env create -f requirements.yml
 conda activate StudyAssistant
 pip install -r requirements.txt
-python -m nltk.downloader punkt stopwords
+python -m nltk.downloader punkt stopwords punkt_tab
 
 pause
