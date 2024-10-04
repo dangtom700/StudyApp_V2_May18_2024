5ec5506c62ed9fb1f418b58f29d94c8c50dbb4d0
Azalea1047 <dangtom700@gmail.com>
2024-10-04 14:26:38 -0700

    convert Python code to C++

---
+++ src/lib/updateDB.hpp
@@ -0,0 +1,13 @@
+#ifndef UPDATE_INFO
+#define UPDATE_INFO
+
+#include <sqlite3.h>
+#include <string>
+#include <filesystem>
+#include <iostream>
+
+namespace UPDATE_INFO {
+
+}
+
+#endif // UPDATE_INFO
