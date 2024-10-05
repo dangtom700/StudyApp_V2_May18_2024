f06fc21a6f0a4098a9edc97586b57143dc422bcd
Azalea1047 <dangtom700@gmail.com>
2024-10-05 16:26:13 -0700

    document the tested and working ones

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 #include "lib/utilities.hpp"
 
 
-void option0(void){
+void option0(void){ // Up and running, memory saved, optimized
     std::cout << "This program is created as an integrated part of the word tokenizer project\n" \
     "to compute the relational distance of each token in a given JSON file.\n" \
     "The relational distance is the Euclidean norm of the vector of token frequencies.\n" \
@@ -16,7 +16,7 @@ void option0(void){
     "The C++ program is written to resolve this issue without using external libraries." << std::endl;
 }
 
-void option1(void){
+void option1(void){// Up and running, memory saved, optimized for data insertion speed and efficiency
     std::vector<std::filesystem::path> filtered_files = UTILITIES_HPP::Basic::extract_data_files(ENV_HPP::json_path, false, ".json");
 
     std::cout << "Computing relational distance data..." << std::endl;
@@ -24,7 +24,7 @@ void option1(void){
     std::cout << "Finished: Relational distance data computed" << std::endl;
 }
 
-void option2(void){
+void option2(void){// Not yet optimized
     std::vector<std::filesystem::path> filtered_files = UTILITIES_HPP::Basic::extract_data_files(ENV_HPP::resource_path, false, ".pdf");
 
     std::cout << "Updating database information..." << std::endl;
@@ -38,11 +38,11 @@ int main() {
     Starting the program with showing the functionality of the program
     Then show the options of the program with configurable choices
     */
-   std::cout << "Starting program..." << std::endl;
-   std::cout << "Options: " << std::endl;
-   std::cout << "0. Help" << std::endl;
-   std::cout << "1. Compute relational distance" << std::endl;
-   std::cout << "2. Update database information" << std::endl;
+    std::cout << "Starting program..." << std::endl;
+    std::cout << "Options: " << std::endl;
+    std::cout << "0. Help" << std::endl;
+    std::cout << "1. Compute relational distance" << std::endl;
+    std::cout << "2. Update database information" << std::endl;
 
     int option = 0;
     std::cout << "Please select an option: ";
