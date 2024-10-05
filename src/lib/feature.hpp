5d9646c24a6c5a27d2eafbcd71a55b9d0d7257fe
Azalea1047 <dangtom700@gmail.com>
2024-10-05 13:37:07 -0700

    quick draft of how the function looks like

--- src/lib/feature.hpp
+++ src/lib/feature.hpp
@@ -11,6 +11,7 @@
 #include "utilities.hpp"
 #include "env.hpp"
 #include "transform.hpp"
+#include "updateDB.hpp"
 
 namespace FEATURE {
     /**
@@ -20,7 +21,8 @@ namespace FEATURE {
      *
      * @param filtered_files A vector of file paths to process.
      */
-    void computeRelationalDistance(const std::vector<std::filesystem::path>& filtered_files, bool show_progress = true) {
+    void computeRelationalDistance(const std::vector<std::filesystem::path>& filtered_files,
+                                    const bool show_progress = true) {
         try {
             bool trigger_once = true;
             for (const std::filesystem::path& file : filtered_files) {
@@ -50,8 +52,56 @@ namespace FEATURE {
         }        
     }
 
-    void populate_database(sqlite3* db, std::vector<std::filesystem::path> filtered_files) {
+    void computeResourceData(const std::vector<std::filesystem::path>& filtered_files,
+                             const bool& show_progress = true,
+                             const bool& reset_table = true) {
+        try {
+            // Open database
+            sqlite3* db = nullptr;
+            if (sqlite3_open(ENV_HPP::data_dumper_path.string().c_str(), &db) != SQLITE_OK) {
+                std::cerr << "Could not open database" << std::endl;
+                return;
+            }
 
+            if (reset_table) {
+                // Drop the table if it already exists (table name: data_info)
+                sqlite3_exec(db, "DROP TABLE IF EXISTS data_info;", nullptr, nullptr, nullptr);
+
+                // Create the table (table name: data_info)
+                std::string command = "CREATE TABLE data_info()" \
+                                    "id TEXT PRIMARY KEY," \
+                                    "file_name TEXT," \
+                                    "file_path TEXT," \
+                                    "last_write_time TEXT," \
+                                    "epoch_time INTEGER," \
+                                    "chunk_count INTEGER," \
+                                    "starting_id INTEGER," \
+                                    "ending_id INTEGER);";
+                sqlite3_exec(db,command.c_str(), nullptr, nullptr, nullptr);
+            }
+
+            for (const std::filesystem::path& file : filtered_files) {
+                DataInfo info = {
+                    .file_name = file.filename().string(),
+                    .file_path = file.string(),
+                    .last_write_time = get_last_write_time(file),
+                    .epoch_time = std::chrono::duration_cast<std::chrono::milliseconds>(std::filesystem::last_write_time(file).time_since_epoch()).count(),
+                    .chunk_count = UPDATE_INFO::count_chunk_for_each_title(db, file.string()),
+                    .starting_id = UPDATE_INFO::get_starting_id(db, file.string()),
+                    .ending_id = UPDATE_INFO::get_ending_id(db, file.string()),
+                };
+
+                info.id = UPDATE_INFO::create_unique_id(file, info.epoch_time, info.chunk_count, info.starting_id);
+                std::cout << "Processed: " << file << " with id: " << info.id << std::endl;
+                UPDATE_INFO::insert_data_info(db, info);
+            }
+            sqlite3_close(db);
+            if (show_progress) {
+                std::cout << "Computing resource data finished" << std::endl;
+            }
+        } catch (const std::exception& e) {
+            std::cerr << "Error: " << e.what() << std::endl;
+        }
     }
 }
 
