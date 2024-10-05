582cfe95118ca1644e8c40a2861fd864080eb1d0
Azalea1047 <dangtom700@gmail.com>
2024-10-05 14:39:54 -0700

    making options to mock to CLI application

--- src/main.cpp
+++ src/main.cpp
@@ -8,21 +8,56 @@
 #include "lib/utilities.hpp"
 #include "lib/updateDB.hpp"
 
-int main() {
 
+void option0(void){
+    std::cout << "This program is created as an integrated part of the word tokenizer project" \
+    "to compute the relational distance of each token in a given JSON file." \
+    "The relational distance is the Euclidean norm of the vector of token frequencies." \
+    "While Python provides a wide range of Natural Language Processing libraries, Python is not so fast at number crunching and heavy data processing." \
+    "The C++ program is written to resolve this issue." << std::endl;
+}
+
+void option1(void){
     std::vector<std::filesystem::path> filtered_files = UTILITIES_HPP::Basic::extract_data_files(ENV_HPP::json_path, true, ".json");
-    
-    std::cout << "Number of files: " << filtered_files.size() << std::endl;
 
     std::cout << "Computing relational distance data..." << std::endl;
     FEATURE::computeRelationalDistance(filtered_files, false);
     std::cout << "Finished: Relational distance data computed" << std::endl;
+}
 
-    filtered_files = UTILITIES_HPP::Basic::extract_data_files(ENV_HPP::resource_path, true, ".pdf");
+void option2(void){
+    
+}
 
-    std::cout << "Computing resource data..." << std::endl;
-    FEATURE::computeResourceData(filtered_files, false);
-    std::cout << "Finished: Resource data computed" << std::endl;
+int main() {
+
+    /*
+    Starting the program with showing the functionality of the program
+    Then show the options of the program with configurable choices
+    */
+   std::cout << "Starting program..." << std::endl;
+   std::cout << "Options: " << std::endl;
+   std::cout << "0. Help" << std::endl;
+   std::cout << "1. Compute relational distance" << std::endl;
+   std::cout << "2. Update database information" << std::endl;
+
+    int option = 0;
+    std::cin >> option;
+
+    switch (option) {
+        case 0:
+            option0();
+            break;
+        case 1:
+            option1();
+            break;
+        case 2:
+            option2();
+            break;
+        default:
+            std::cout << "Invalid option" << std::endl;
+            break;
+    }
 
     return 0; // End of program
 }
