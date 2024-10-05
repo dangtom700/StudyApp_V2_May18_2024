f43e13125557ea66351fd285baf1a6b69ed8fe5d
Azalea1047 <dangtom700@gmail.com>
2024-10-05 13:36:26 -0700

    add another function to update database

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,28 @@
 #include <iostream>
 #include <filesystem>
 #include <vector>
+#include <sqlite3.h>
 
 #include "lib/feature.hpp"
 #include "lib/env.hpp"
 #include "lib/utilities.hpp"
+#include "lib/updateDB.hpp"
 
 int main() {
-    std::filesystem::path target_folder = ENV_HPP::json_path;
-    std::vector<std::filesystem::path> json_files = UTILITIES_HPP::Basic::list_directory(target_folder, false);
-    std::vector<std::filesystem::path> filtered_files = UTILITIES_HPP::Basic::filter_by_extension(json_files, ".json");
+
+    std::vector<std::filesystem::path> filtered_files = UTILITIES_HPP::Basic::extract_data_files(ENV_HPP::json_path, true, ".json");
     
     std::cout << "Number of files: " << filtered_files.size() << std::endl;
 
     std::cout << "Computing relational distance data..." << std::endl;
     FEATURE::computeRelationalDistance(filtered_files, false);
     std::cout << "Finished: Relational distance data computed" << std::endl;
+
+    filtered_files = UTILITIES_HPP::Basic::extract_data_files(ENV_HPP::resource_path, true, ".pdf");
+
+    std::cout << "Computing resource data..." << std::endl;
+    FEATURE::computeResourceData(filtered_files, false);
+    std::cout << "Finished: Resource data computed" << std::endl;
 
     return 0; // End of program
 }
