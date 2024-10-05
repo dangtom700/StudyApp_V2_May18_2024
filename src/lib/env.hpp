7e6f337233e12f8838fd7ee6710f459994e1c2c8
Azalea1047 <dangtom700@gmail.com>
2024-10-05 11:24:41 -0700

    add resource path way

--- src/lib/env.hpp
+++ src/lib/env.hpp
@@ -4,6 +4,9 @@
 #include <filesystem>
 
 namespace ENV_HPP {
+    // source paths
+    std::filesystem::path resource_path = "D:\\READING LIST";
+
     // get the main.cpp working directory, then go up one level, go onw level down to data folder
     std::filesystem::path data_root = std::filesystem::current_path() / ("data");
 
